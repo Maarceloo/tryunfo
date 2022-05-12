c2744ca978ea16ddc596178bd8ac5f4eb418d751
Marcelo De Lima <delima_marcelo@outlook.com>
2022-05-12 18:02:10 -0300

    04 Finalizado

--- src/App.js
+++ src/App.js
@@ -3,15 +3,52 @@ import Card from './components/Card';
 import Form from './components/Form';
 
 class App extends React.Component {
+  state = {
+    nome: 'xablau',
+    descricao: '',
+    attr1: '',
+    attr2: '',
+    attr3: '',
+    imge: '',
+    raridade: '',
+    check: false,
+  };
+
+  dadosInput = ({ target }) => {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({ [name]: value });
+  };
+
   render() {
+    const { nome, descricao, attr1, attr2, attr3, imge, raridade, check } = this.state;
     return (
       <div>
         <h1>Tryunfo</h1>
         <div>
-          <Form />
+          <Form
+            cardName={ nome }
+            cardDescription={ descricao }
+            cardAttr1={ attr1 }
+            cardAttr2={ attr2 }
+            cardAttr3={ attr3 }
+            cardImage={ imge }
+            cardRare={ raridade }
+            cardTrunfo={ check }
+            onInputChange={ this.dadosInput }
+          />
         </div>
         <div>
-          <Card />
+          <Card
+            cardName={ nome }
+            cardDescription={ descricao }
+            cardAttr1={ attr1 }
+            cardAttr2={ attr2 }
+            cardAttr3={ attr3 }
+            cardImage={ imge }
+            cardRare={ raridade }
+            cardTrunfo={ check }
+          />
         </div>
       </div>
     );
